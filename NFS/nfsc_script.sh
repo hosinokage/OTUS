15f24d58ab22dffffc826d1c5803d146dc6f869a
hosinokage <hosinokage@gmail.com>
2024-10-17 23:31:17 +0300

    Add files via upload

---
+++ NFS/nfsc_script.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+sudo apt install nfs-common
+
+echo "192.168.50.10:/srv/share/ /mnt nfs vers=3,noauto,x-systemd.automount 0 0" >> /etc/fstab
+
+systemctl daemon-reload
+systemctl restart remote-fs.target
