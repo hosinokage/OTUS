a9557c59e2e49d48620f27cdb2440af5187c6f4f
hosinokage <hosinokage@gmail.com>
2024-11-21 23:32:11 +0300

    Add files via upload

---
+++ Bash/taskbash.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Log file check
+if [ ! -r "$1" ] ; then 
+	echo "Error: log file $1 not found." >&2
+	exit 1
+fi
+
+# lockfile
+lock_file=./lock_file
+if [ -f $lock_file ]; then
+    echo Wait , job is already started\!
+    exit 1
+else
+    echo "PID: $$" > $lock_file
+    trap 'rm -f "$lock_file"; exit $?' INT TERM EXIT
+fi
+touch $lock_file
+
+
+
+time=$(cat access-4560-644067.log | awk '{print $4}' | head -n 1 &&  date | awk '{print $2,$3,$4,$6}')
+IP=$(cat access-4560-644067.log | awk '{print $1}' | sort | uniq -c | sort -rn | head -n 10)  
+URL=$(cat access-4560-644067.log | awk '{print $7}' | sort | uniq -c | sort -rn | head -n 10)
+ERR=$(cat access-4560-644067.log | awk '{print $9}' | grep ^4 | sort | uniq -d -c | sort -rn) 
+CODE=$(cat access-4560-644067.log | awk '{print $9}'| grep -v "-" | sort | uniq -c | sort -rn | head -n 10)
+
+echo -e "Date:$time\n"IP addresses"\n$IP\n\n"URLs"\n$URL\n\n"Errors"\n$ERR\n\n"HTTP Responses"\n$CODE\n" | mail -s "Server Logs" email@domain.com
+ 
+# release lock
+rm -f $lock_file
+trap - INT TERM EXIT
